9e35c724996c70c0103fee7564e2b19d91a10208
GoldenShiber <johan.soodla@gmail.com>
2024-01-23 17:48:30 +0100

    lol something happened

--- AdventOfCode2023/src/main/java/org/example/solutions/DayTwo2023.java
+++ AdventOfCode2023/src/main/java/org/example/solutions/DayTwo2023.java
@@ -9,66 +9,101 @@ import java.util.logging.Logger;
 
 public class DayTwo2023 {
     private static final Logger logger = Logger.getLogger(DayTwo2023.class.getName());
-    private static List<String> fileOutput;
-
-    // Need to find all indexes of a word in String
     private static HashMap<Integer, String> gameMap;
-
-    public static void solution(){
-        gameMap.forEach((k,v) -> System.out.printf("hej"));
-    }
+    private static HashMap<String, Integer> colorMap;
+    private static final int RED = 12;
+    private static final int BLUE = 14;
+    private static final int GREEN = 13;
 
     // The idea of the game is to check if the amount of cubes taken each time is lower than the highest possible amount
-    private static void getGameIndexSum(int red, int green, int blue,List<String> bricksList){
+    private static void getGameIndexSum(List<String> bricksList){
         int gameIndex = 1;
         int sumOfGameIndex = 0;
         for(String bricks : bricksList){
-            sumOfGameIndex += isBrickSetPossible(red, green, blue, bricks)? gameIndex : 0;
+            sumOfGameIndex += isBrickSetPossible(bricks)? gameIndex : 0;
             gameIndex++;
         }
         logger.info(String.format("The sum of the game index is: %d", sumOfGameIndex));
     }
 
-    private static boolean isBrickSetPossible(int red, int green, int blue, String brickSet){
+    private static void getMultipleValue( List<String> brickList){
+        int sumOfGame = 0;
+        for(String bricks : brickList){
+            colorMap = new HashMap<>();
+            colorMap.put("red", 0);
+            colorMap.put("blue", 0);
+            colorMap.put("green", 0);
+            sumOfGame += getValueForBrickSet( bricks);
+        }
+        logger.info(String.format("The sum of the game is: %d", sumOfGame));
+
+    }
+
+    private static void updateColorValues( String bricks){
+        List<String> bricksInfo = Arrays.stream(bricks.trim().split(" ")).toList();
+        String color = bricksInfo.get(1);
+        switch (color) {
+            case "blue":
+                    colorMap.put("blue", Math.max(colorMap.get("blue"), Integer.parseInt(bricksInfo.getFirst())));
+                break;
+            case "red":
+                    colorMap.put("red", Math.max(colorMap.get("red"), Integer.parseInt(bricksInfo.getFirst())));
+                break;
+            case "green":
+                    colorMap.put("green", Math.max(colorMap.get("green"), Integer.parseInt(bricksInfo.getFirst())));
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static boolean isBrickSetPossible(String brickSet){
         String[] a = brickSet.replace(";", ",").split(",");
         List<Boolean> logicList = new ArrayList<>();
-        Arrays.stream(a).toList().forEach(string -> logicList.add(isBrickColorPossible(string, red, green, blue)));
+        Arrays.stream(a).toList().forEach(string -> logicList.add(isBrickColorPossible(string)));
 
         return !logicList.contains(false);
     }
 
-    private static boolean isBrickColorPossible(String bricks, int red, int green, int blue){
+    private static int getValueForBrickSet( String brickSet){
+        String[] a = brickSet.replace(";", ",").split(",");
+        Arrays.stream(a).forEach(DayTwo2023::updateColorValues);
+        return colorMap.get("red")*colorMap.get("blue")*colorMap.get("green");
+    }
+
+    private static boolean isBrickColorPossible(String bricks){
         int brickLimit = 0;
         List<String> bricksInfo = Arrays.stream(bricks.trim().split(" ")).toList();
         String color = bricksInfo.get(1);
         switch (color) {
             case "blue":
-                brickLimit = blue;
+                brickLimit = BLUE;
                 break;
             case "red":
-                brickLimit = red;
+                brickLimit = RED;
                 break;
             case "green":
-                brickLimit = green;
+                brickLimit = GREEN;
                 break;
             default:
                 break;
         }
-        return  Integer.valueOf(bricksInfo.get(0)) <= brickLimit;
+        return  Integer.parseInt(bricksInfo.get(0)) <= brickLimit;
     }
 
     public static void main(String[] args) throws IOException {
         InputStream inputStream = Day1.class.getResourceAsStream("/informationFiles/input23Day2.txt");
         //InputStream inputStream = Day1.class.getResourceAsStream("/informationFiles/input23Day2Test.txt");
-        fileOutput= FileHandling.readFromInputAndMakeList(inputStream);
+        List<String> fileOutput = FileHandling.readFromInputAndMakeList(inputStream);
         gameMap = new HashMap<>();
         fileOutput.forEach(line -> gameMap.put(Integer.valueOf(Arrays.stream(line.split(":")).findFirst().get().replace("Game", "").trim()), Arrays.stream(line.split(":")).toList().get(1)));
+        List<String> gameList = gameMap.values().stream().toList();
         long startTime = System.nanoTime();
-        getGameIndexSum(12, 13, 14,gameMap.values().stream().toList());
+        getGameIndexSum(gameList);
         long endTime = System.nanoTime();
         logger.info(String.format("Problem one took %d milliseconds", ((endTime-startTime)/100000)));
         startTime = System.nanoTime();
-        //solution(convertToNumberList());
+        getMultipleValue(gameList);
         endTime = System.nanoTime();
         logger.info(String.format("Problem two took %d milliseconds", ((endTime-startTime)/100000)));
     }
